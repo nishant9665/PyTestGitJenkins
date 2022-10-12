696afa27e0cc375149428d0d82adf352919953f6
Nishant Narwade <nishant.narwade@blueconchtech.com>
2022-10-12 13:01:14 +0530

    first commit

---
+++ Pytest_Session2/test_google.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+
+
+driver = None
+
+
+def setup_module(module):
+    global driver
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.implicitly_wait(10)
+    driver.delete_all_cookies()
+    driver.get("http://www.google.com")
+
+
+def teardown_module(module):
+    driver.quit()
+
+
+def test_google_title():
+    assert driver.title == "Google"
